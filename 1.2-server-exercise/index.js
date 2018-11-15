8cba5c3869dfa0a16b4dd0834d4a8c9d089b677a
Juliano Rodrigues <juliano.silva.rodrigues@gmail.com>
2018-11-15 22:02:33 +0000

    Server Exercise with Framwork Object

--- 1.2-server-exercise/index.js
+++ 1.2-server-exercise/index.js
@@ -1,14 +1,45 @@
 const { ApolloServer, gql } = require("apollo-server");
 
-const frameworks = [
+const Frameworks = [
   {
+    id: 1,
     title: "React",
     git: "https://github.com/facebook/react/",
     stars: 104170
   },
   {
+    id: 2,
     title: "Vue",
     git: "https://github.com/vuejs/vue/",
     stars: 104483
   }
 ];
+
+const typeDefs = gql`
+  type Framework {
+    id: ID!,
+    title: String,
+    git: String,
+    stars: Int
+  }
+  type Query {
+    allFrameworks: [Framework]
+    framework (id: ID): Framework
+  }
+`
+
+const resolvers = {
+  Query: {
+    allFrameworks: () => Frameworks,
+    framework: (_, { id }) => {
+      return Frameworks.find(framework => framework.id === parseInt(id, 10));
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.listen().then(data => console.log(data.url));
